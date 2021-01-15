b3011985b023b9a69e38bc19a823ad60eee97e83
dmytrenkojulia <dmytrenkojulia@gmail.com>
2021-01-15 05:15:48 +0100

    style: fix image padding

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,7 +5,7 @@
 			$plugins = array("Plugin1", "Plugin2", "Plugin3");
 			foreach ($plugins as $plugin) { ?>
 				<div class="card mb-3" style="max-width: 540px;">
-					<div class="row g-0 pl-3">
+					<div class="row g-0 pl-3 pt-3 pt-md-0">
 						<div class="col-md-4 d-flex">
 							<img src="https://camo.githubusercontent.com/9f29e3d7fc79b01485632a99af0fd6a0f65d921adc720d0d30cdce9475caa9ca/68747470733a2f2f6769746875622e7375726d6f6e2e6d652f696d616765732f636f6d6d6f6e2f7377697065722d6c6f676f2e737667" alt="Swiper.js logo">
 						</div>
