e0f2d5059d45a0d37984ce961354fef1870b1f16
dmytrenkojulia <dmytrenkojulia@gmail.com>
2021-01-15 04:58:40 +0100

    feat: add foreach ($plugins as $plugin)

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,17 +1,22 @@
 <h2><?= $title ?></h2>
 <p>Strona główna</p>
 
-<div class="card mb-3" style="max-width: 540px;">
-	<div class="row g-0 pl-3">
-		<div class="col-md-4 d-flex">
-			<img src="https://camo.githubusercontent.com/9f29e3d7fc79b01485632a99af0fd6a0f65d921adc720d0d30cdce9475caa9ca/68747470733a2f2f6769746875622e7375726d6f6e2e6d652f696d616765732f636f6d6d6f6e2f7377697065722d6c6f676f2e737667" alt="Swiper.js logo">
-		</div>
-		<div class="col-md-8">
-			<div class="card-body">
-				<h5 class="card-title">Card title</h5>
-				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-				<button type="button" class="btn btn-primary">Primary</button>
-			</div>
-		</div>
-	</div>
-</div>
+		<?php
+			$plugins = array("Plugin1", "Plugin2", "Plugin3");
+			foreach ($plugins as $plugin) { ?>
+				<div class="card mb-3" style="max-width: 540px;">
+					<div class="row g-0 pl-3">
+						<div class="col-md-4 d-flex">
+							<img src="https://camo.githubusercontent.com/9f29e3d7fc79b01485632a99af0fd6a0f65d921adc720d0d30cdce9475caa9ca/68747470733a2f2f6769746875622e7375726d6f6e2e6d652f696d616765732f636f6d6d6f6e2f7377697065722d6c6f676f2e737667" alt="Swiper.js logo">
+						</div>
+						<div class="col-md-8">
+							<div class="card-body">
+								<h5 class="card-title"> <?php echo $plugin ?></h5>
+								<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+								<button type="button" class="btn btn-primary">Więcej</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php }
+		?>
