ff25c5d3a743d66f793958218a0d09adcd16da82
dmytrenkojulia <dmytrenkojulia@gmail.com>
2021-01-17 20:20:48 +0100

    refactor: delete a button and add btn bootstrap class to a link

--- application/views/components/post_tile.component.php
+++ application/views/components/post_tile.component.php
@@ -12,7 +12,7 @@
 									<h3 class="card-title"> '.$post['title'].'</h3>
 									<small class="post-date">📅 '.$post['created_at'].'</small>
 									<p class="card-text">'.$post['body'].'</p>
-									<a href='.base_url().'posts/'.$post['slug'].'><button type="button" class="btn btn-primary" onclick="myFunction()" >Więcej</button></a>
+									<a class="btn btn-primary" href='.site_url().'posts/'.$post['slug'].'>Więcej</a>
 								</div>
 							</div>
 						</div>
