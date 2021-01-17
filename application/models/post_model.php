ad287dd249405df68a0d332a8ff8c06a8016de32
dmytrenkojulia <dmytrenkojulia@gmail.com>
2021-01-17 15:53:27 +0100

    feat: add function that returns the latest 5 posts

--- application/models/post_model.php
+++ application/models/post_model.php
@@ -13,4 +13,9 @@
 		$query = $this->db->get_where('posts', array('slug' => $slug));
 		return $query->row_array();
 	}
+
+	public function get_latest_posts() {
+		$query = $this->db->query("SELECT * FROM posts ORDER BY created_at DESC LIMIT 5");
+		return $query->result_array();
+	}
  }
