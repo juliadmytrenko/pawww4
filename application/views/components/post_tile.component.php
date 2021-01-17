6ec976bab045ed70e2849f382377d8510b1ee07f
dmytrenkojulia <dmytrenkojulia@gmail.com>
2021-01-17 15:21:53 +0100

    feat: add post_tile.component.php

---
+++ application/views/components/post_tile.component.php
@@ -0,0 +1,23 @@
+<?php
+	function createPostTile($posts) {
+		foreach ($posts as $post) :
+			echo '
+				<div class="card mb-3">
+						<div class="row g-0 pl-4 pt-4 pt-md-0">
+							<div class="col-md-4 d-flex justify-content-center align-items-center p-3">
+								<img style="max-height: 250px" src="https://camo.githubusercontent.com/9f29e3d7fc79b01485632a99af0fd6a0f65d921adc720d0d30cdce9475caa9ca/68747470733a2f2f6769746875622e7375726d6f6e2e6d652f696d616765732f636f6d6d6f6e2f7377697065722d6c6f676f2e737667" alt="Swiper.js logo">
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h3 class="card-title"> '.$post['title'].'</h3>
+									<small class="post-date">📅 '.$post['created_at'].'</small>
+									<p class="card-text">A short description. Mauris facilisis, turpis eget pharetra pulvinar, ex elit fringilla ex, sed aliquam diam ante a nisl. Nam malesuada augue turpis, at mollis turpis gravida ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam volutpat eleifend tortor. Vestibulum varius orci non varius commodo. Nunc vel lorem vitae diam aliquet dictum.</p>
+									<button type="button" class="btn btn-primary">Więcej</button>
+								</div>
+							</div>
+						</div>
+				</div>
+			';
+		endforeach;
+	}
+?>
